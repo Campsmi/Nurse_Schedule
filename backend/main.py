4d0e6be2df3e9fce03be5e7176be1c871c5331fa
Diogo Campas <campasdiogo@gmail.com>
2025-11-27 17:15:38 +0000

    Update main endpoints and database entity

--- backend/main.py
+++ backend/main.py
@@ -1,26 +1,19 @@
+
 from fastapi import FastAPI
-from db import get_connection
-from models import Nurse, Shift
-import psycopg2
+from db import database  # your Database instance from db.py
+from models import Nurse, Shift, mapper_registry
+from sqlalchemy import select
 
 app = FastAPI()
 
+@app.lifespan("startup")
+async def startup():
+    await database.connect()
+
+@app.lifespan("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 @app.get("/")
-def root():
-    return {"message": "Nurse Scheduler is running!"}
-
-@app.get("/nurses")
-def get_nurses():
-    conn = get_connection()
-    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-    cur.execute("SELECT id, full_name, current_hours FROM nurses")
-    rows = cur.fetchall()
-
-    cur.close()
-    conn.close()
-
-    # Convert DB rows → Nurse dataclass instances
-    nurses = [Nurse(id=row["id"], name=row["full_name"], current_hours=row["max_hours_per_week"]) for row in rows]
-
-    return nurses
+async def root():
+    return {"message": "Nurse Scheduler API is running!"}
